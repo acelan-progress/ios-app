702d8deefe368086a16a6c5b96e9bc598cb0aac1
Mikhail Eremeev <meremeev@sfedu.ru>
2023-03-10 20:16:11 +0300

    chore: initialize ios app xcode project

---
+++ Acelan-Progress/Acelan_ProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acelan_ProgressApp.swift
+//  Acelan-Progress
+//
+//  Created by Mikhail Eremeev on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Acelan_ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
